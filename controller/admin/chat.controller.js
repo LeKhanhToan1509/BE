755bd74b409aaaca14a83c7f18fda40a2ca013dc
LeKhanhToan1509 <toanxm1509@gmail.com>
2024-03-28 19:55:54 +0700

    add

--- controller/admin/chat.controller.js
+++ controller/admin/chat.controller.js
@@ -1,38 +1,56 @@
+// Đảm bảo rằng bạn đã require các module cần thiết ở đầu file
 const chat = require("../../models/chat.model");
 const chatUser = require("../../models/account.model");
+
 module.exports.index = async (req, res) => {
-    let user_id = res.locals.user._id;
-    let fullname = res.locals.user.name;
-    _io.once("connection", (socket) => {
-        console.log("Connected user_id:", user_id);
-        socket.on("CLIENT_SEND_MESSAGE", async (message) => {
-            const newChat = new chat({
-                content: message,
-                user_id: user_id,
-            });
+    const user_id = res.locals.user._id;
+    const fullname = res.locals.user.name;
 
-            await newChat.save();
-            _io.emit("SERVER_RETURN_MESSAGE", {
-                userID: user_id,
-                name: fullname,
-                content: message,
-            });
-        });
-    });
+    setupSocketListeners(user_id, fullname);
 
-    const chats = await chat.find({
-        delete: false,
-    });
+    // Logic để lấy dữ liệu chat và gửi dữ liệu đến template Pug
+    const chats = await chat.find({ delete: false }).lean();
+    const userIDs = chats.map((item) => item.user_id);
+    const users = await chatUser.find(
+        { _id: { $in: userIDs } },
+        "name image _id"
+    );
+
     for (const item of chats) {
-        const user = await chatUser
-            .findOne({
-                _id: item.user_id,
-            })
-            .select("name image _id");
-        item.inforUser = user;
+        item.inforUser = users.find((user) => user._id.equals(item.user_id));
     }
 
-    res.render("admin/pages/chat/chat.pug", {
-        chat: chats,
-    });
+    res.render("admin/pages/chat/chat.pug", { chat: chats });
 };
+
+function setupSocketListeners(user_id, fullname) {
+    _io.once("connection", (socket) => {
+        console.log("Connected user_id:", user_id);
+
+        socket.on("CLIENT_SEND_MESSAGE", (message) =>
+            handleSendMessage(user_id, fullname, message)
+        );
+        socket.on("CLIENT_SEND_TYPING", (type) =>
+            handleSendTyping(socket, user_id, fullname, type)
+        );
+    });
+}
+
+async function handleSendMessage(user_id, fullname, message) {
+    const newChat = new chat({ content: message, user_id });
+    await newChat.save();
+    _io.emit("SERVER_RETURN_MESSAGE", {
+        userID: user_id,
+        name: fullname,
+        content: message,
+    });
+}
+
+function handleSendTyping(socket, user_id, fullname, type) {
+    socket.broadcast.emit("SERVER_RETURN_TYPING", {
+        name: fullname,
+        userID: user_id,
+        type,
+    });
+    console.log(type);
+}
--- public/admin/js/chat.js
+++ public/admin/js/chat.js
@@ -1,35 +1,96 @@
-const formSendData = document.querySelector("#send-message");
-if (formSendData) {
-    formSendData.addEventListener("submit", (e) => {
-        e.preventDefault();
-        if (e.target.elements.message.value.trim() != "") {
-            const content = e.target.elements.message.value;
-            socket.emit("CLIENT_SEND_MESSAGE", content);
-            e.target.elements.message.value = "";
-        } else {
-            e.target.elements.message.value = "";
-        }
-    });
+document.addEventListener("DOMContentLoaded", () => {
+    setupChatForm();
+    setupMessageScroll();
+    setupServerMessageListener();
+    setupEmojiPicker();
+    setupInputChat();
+    setupTypingIndicator();
+});
+
+function setupChatForm() {
+    const formSendData = document.querySelector("#send-message");
+    if (formSendData) {
+        formSendData.addEventListener("submit", handleChatSubmit);
+    }
 }
-document.addEventListener("DOMContentLoaded", (event) => {
+
+function handleChatSubmit(e) {
+    e.preventDefault();
+    const messageInput = e.target.elements.message;
+    if (messageInput.value.trim() !== "") {
+        socket.emit("CLIENT_SEND_MESSAGE", messageInput.value.trim());
+        messageInput.value = "";
+    }
+}
+
+function setupMessageScroll() {
     const messagesContainer = document.querySelector(".messages");
     if (messagesContainer) {
         messagesContainer.scrollTop = messagesContainer.scrollHeight;
     }
-});
+}
 
-socket.on("SERVER_RETURN_MESSAGE", (data) => {
-    const id = document.querySelector("[my-id]").getAttribute("my-id");
-    const message = document.querySelector(".messages");
-    const div = document.createElement("div");
-    div.classList.add("message");
-    if (id == data.userID) {
-        div.classList.add("outgoing");
+function setupServerMessageListener() {
+    socket.on("SERVER_RETURN_MESSAGE", handleServerReturnMessage);
+}
+
+function handleServerReturnMessage(data) {
+    const myId = document.querySelector("[my-id]")?.getAttribute("my-id");
+    const messages = document.querySelector(".messages");
+    const box = document.querySelector(".typing");
+    if (messages) {
+        const div = document.createElement("div");
+        div.classList.add(
+            "message",
+            data.userID === myId ? "outgoing" : "incoming"
+        );
         div.innerHTML = `<div class="content">${data.content}</div>`;
-    } else {
-        div.classList.add("incoming");
-        div.innerHTML = `<div class="content">${data.content}</div>`;
+        messages.insertBefore(div, box);
+        messages.scrollTop = messages.scrollHeight;
     }
-    message.appendChild(div);
-    message.scrollTop = message.scrollHeight;
-});
+}
+
+function setupEmojiPicker() {
+    const emojiPicker = document.querySelector("emoji-picker");
+    if (emojiPicker) {
+        emojiPicker.addEventListener("emoji-click", (e) => {
+            const input = document.querySelector(".value");
+            if (input) {
+                input.value += e.detail.unicode;
+            }
+        });
+    }
+}
+
+function setupInputChat() {
+    let timeout;
+    const inputChat = document.querySelector(".value");
+    if (inputChat) {
+        inputChat.addEventListener("keyup", () => {
+            socket.emit("CLIENT_SEND_TYPING", "show");
+            clearTimeout(timeout);
+            timeout = setTimeout(
+                () => socket.emit("CLIENT_SEND_TYPING", "hide"),
+                1500
+            );
+        });
+    }
+}
+
+function setupTypingIndicator() {
+    let isTypingIndicatorShown = false;
+    socket.on("SERVER_RETURN_TYPING", (data) => {
+        const typingIndicator = document.querySelector(".typing");
+        if (typingIndicator) {
+            if (data.type === "show" && !isTypingIndicatorShown) {
+                typingIndicator.innerHTML = `<div class="typing-indicator"><span></span><span></span><span></span></div>`;
+                isTypingIndicatorShown = true;
+            } else if (data.type === "hide" && isTypingIndicatorShown) {
+                typingIndicator.innerHTML = "";
+                isTypingIndicatorShown = false;
+            }
+        }
+        const messages = document.querySelector(".messages");
+        messages.scrollTop = messages.scrollHeight;
+    });
+}
